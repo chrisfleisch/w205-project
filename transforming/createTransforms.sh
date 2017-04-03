cb53b24b6fcba31d552ddc9c561278a9da74edd3
Chris Fleisch <cfleisch@berkeley.edu>
2017-04-03 20:43:29 +0000

    Moved export tables to createTransform.sh

--- transforming/createTransforms.sh
+++ transforming/createTransforms.sh
@@ -36,3 +36,11 @@ for column in $FLOATVARS; do
   sed -i "s/ $column,/ cast\($column as float\) as $column,/Ig" $TEMPLATE
   sed -i "s/ $column FROM/ cast\($column as float\) as $column FROM/Ig" $TEMPLATE
 done
+
+# Add reddit_bottles and whiskey_business
+printf "\nDROP TABLE reddit_bottles;\n" >> $TEMPLATE
+printf "CREATE TABLE reddit_bottles AS SELECT altbrand, sum(Rating)/count(Rating) as avg_rating, count(Rating) as num_reviews FROM redditarchive WHERE rating > 0 GROUP BY altbrand;\n\n"  >> $TEMPLATE
+
+printf "DROP TABLE whiskey_business;\n"  >> $TEMPLATE
+printf "CREATE TABLE whiskey_business AS SELECT  V.brand, V.oz as size,  sum(R.avg_rating)/count(avg_rating) as avg_rating, sum(MetaCritic)/count(MetaCritic) as critic_score,  V.altprice, sum(V.altprice)/sum(V.oz) as price_per_oz  from vaprices V LEFT JOIN reddit_bottles  R on V.altbrand=R.altbrand LEFT JOIN metacritic M on V.altbrand=M.altbrand GROUP BY V.brand, V.altprice, V.oz;\n\n"  >> $TEMPLATE
+
