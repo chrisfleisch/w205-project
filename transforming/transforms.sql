c0ce41b3cd45669150d152d9c31f3faebbf700f0
cathbar <dylan.kenny.drexel@gmail.com>
2017-04-19 00:32:42 +0000

    Removed forbidden characters from secondtransforms.sql

--- transforming/transforms.sql
+++ transforming/transforms.sql
@@ -13,8 +13,5 @@ CREATE TABLE vaprices AS SELECT description, code, brand, size, age, cast(Proof 
 
 
 DROP TABLE reddit_bottles;
-CREATE TABLE reddit_bottles AS SELECT altbrand, sum(Rating)/count(Rating) as avg_rating, count(Rating) as num_reviews FROM redditarchive WHERE rating > 0 GROUP BY altbrand;
-
-DROP TABLE whiskey_business;
-CREATE TABLE whiskey_business AS SELECT  V.brand, V.oz as size,  sum(R.avg_rating)/count(avg_rating) as avg_rating, sum(MetaCritic)/count(MetaCritic) as critic_score,  V.altprice, sum(V.altprice)/sum(V.oz) as price_per_oz  from vaprices V LEFT JOIN reddit_bottles  R on V.altbrand=R.altbrand LEFT JOIN metacritic M on V.altbrand=M.altbrand GROUP BY V.brand, V.altprice, V.oz;
+CREATE TABLE reddit_bottles AS SELECT altbrand, sum(Rating)/count(Rating) as avg_rating, count(Rating) as num_reviews, sum(altbottleprice)/count(altbottleprice) as altbottleprice FROM redditarchive WHERE rating > 0 GROUP BY altbrand;
 
